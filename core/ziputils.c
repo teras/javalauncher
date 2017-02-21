5b0350fdb23a762bfd12db3f8e02fd97fd1c80f2
Panayotis Katsaloulis <panayotis@panayotis.com>
2017-02-21 17:19:39 +0200

    improved handling of zip file entries

--- core/ziputils.c
+++ core/ziputils.c
@@ -4,21 +4,22 @@
 char* getEntry(const char *zipfile, const char* zipentryname, int* const isvalid) {
     unzFile * file = unzOpen(zipfile);
     if (file != NULL) {
+        char * data = NULL;
         if (unzLocateFile(file, zipentryname, NULL) == UNZ_OK) {
-            unz_file_info info;
-            unzGetCurrentFileInfo(file, &info, NULL, 0, NULL, 0, NULL, 0);
             if (isvalid)
                 *isvalid = 1;
+            unz_file_info info;
+            unzGetCurrentFileInfo(file, &info, NULL, 0, NULL, 0, NULL, 0);
             if (info.uncompressed_size > 0 && unzOpenCurrentFile(file) == UNZ_OK) {
-                char * data = malloc(info.uncompressed_size + 1);
+                data = malloc(info.uncompressed_size + 1);
                 unzReadCurrentFile(file, data, info.uncompressed_size);
                 data[info.uncompressed_size] = 0;
                 if (unzCloseCurrentFile(file) == UNZ_CRCERROR)
                     fprintf(stderr, "Data was read correctly but the CRC does not match");
-                return data;
             }
         }
         unzClose(file);
+        return data;
     }
     if (isvalid)
         *isvalid = 0;
