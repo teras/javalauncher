36787cee71adb8329e3644f0d39e06f23723d7d8
Panayotis Katsaloulis <panayotis@panayotis.com>
2017-02-21 17:16:36 +0200

    getEntry in ziputils now properly informs if a zip entry file is found

--- core/ziputils.c
+++ core/ziputils.c
@@ -6,22 +6,21 @@ char* getEntry(const char *zipfile, const char* zipentryname, int* const isvalid
     if (file != NULL) {
         if (unzLocateFile(file, zipentryname, NULL) == UNZ_OK) {
             unz_file_info info;
-            unzGetCurrentFileInfo (file, &info, NULL, 0, NULL, 0, NULL, 0);
+            unzGetCurrentFileInfo(file, &info, NULL, 0, NULL, 0, NULL, 0);
+            if (isvalid)
+                *isvalid = 1;
             if (info.uncompressed_size > 0 && unzOpenCurrentFile(file) == UNZ_OK) {
                 char * data = malloc(info.uncompressed_size + 1);
                 unzReadCurrentFile(file, data, info.uncompressed_size);
                 data[info.uncompressed_size] = 0;
                 if (unzCloseCurrentFile(file) == UNZ_CRCERROR)
-                    fprintf(stderr, "Data was rad correctly but the CRC does not match");
+                    fprintf(stderr, "Data was read correctly but the CRC does not match");
                 return data;
             }
         }
         unzClose(file);
-        if (isvalid)
-            *isvalid = 1;
-    } else {
-        if(isvalid)
-            *isvalid = 0;
     }
+    if (isvalid)
+        *isvalid = 0;
     return NULL;
 }
