97cbc02707c014174f89c79fcd139af4f0fddc25
Panayotis Katsaloulis <panayotis@panayotis.com>
2019-06-03 17:26:03 +0300

    show debug only if env DEBUG is present

--- debug.nim
+++ debug.nim
@@ -1,5 +1,10 @@
+import os
+
+let shouldDebug = existsEnv("DEBUG")
+
 proc debug*(message: string): void =
-    echo message
+    if shouldDebug:
+        echo message
     return
 
 proc error*(message: string): void =
