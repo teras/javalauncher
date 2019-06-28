67c77a04e412bd182fdac249f2b2f8641e741609
Panayotis Katsaloulis <panayotis@panayotis.com>
2019-06-28 13:19:52 +0300

    set carver optional

--- javalauncher.nim
+++ javalauncher.nim
@@ -1,4 +1,7 @@
-import hound, carver, debug, ospaths, os, proclauncher
+const USE_CARVER = true
+
+import hound, debug, ospaths, os, proclauncher
+when USE_CARVER: import carver
 from strutils import replace
 
 let javabin = find_java()
@@ -7,12 +10,12 @@ let java_location = javabin.parentDir
 let jar_location = jar.parentDir
 let launch_location = getAppFilename().parentDir()
 
-proc asArg(item:string) : string =
+proc asArg(item: string): string =
     return item
         .replace("@@JAVA_LOCATION@@", java_location)
         .replace("@@JAR_LOCATION@@", jar_location)
         .replace("@@LAUNCH_LOCATION@@", launch_location)
-proc addArgs(args: var seq[string], list:seq[string]) =
+proc addArgs(args: var seq[string], list: seq[string]) =
     for item in list:
         args.add(item.asArg)
 
@@ -20,13 +23,14 @@ var args: seq[string]
 var vmargs: seq[string]
 var postArgs: seq[string]
 
-let json = extractData(jar)
-if json != "":
-    findArgs(json, vmargs, postArgs)
-args.addArgs(vmargs)
+when USE_CARVER:
+    let json = extractData(jar)
+    if json != "":
+        findArgs(json, vmargs, postArgs)
+    args.addArgs(vmargs)
 args.add("-jar")
 args.add(jar)
-args.addArgs(postArgs)
+when USE_CARVER: args.addArgs(postArgs)
 args.addArgs(commandLineParams())
 
 launch(javabin, args)
